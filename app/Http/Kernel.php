5de5d03d8d302d8fa1d254c433f374c88c3e661d
pramod sharma <pramod.sharma@mindpooltech.in>
2024-04-22 11:44:06 +0530

    commit fix

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -21,7 +21,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        \App\Http\Middleware\CorsMiddleware::class,
+        
     ];
 
     /**
@@ -46,6 +46,12 @@ class Kernel extends HttpKernel
             \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
+    ];
+
+    // Route middleware...
+    protected $routeMiddleware = [
+        'cors' => \App\Http\Middleware\CorsMiddleware::class, // Example middleware
+        // Other route middleware...
     ];
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -29,7 +29,7 @@ Route::group(
         Route::post('login', 'AuthController@login')->name('user.login');
         Route::get("micro-status", fn () => response()->json(["active"]));
 
-        Route::middleware(['auth:sanctum'])
+        Route::middleware(['auth:sanctum','cors'])
             ->group(function () {
                 Route::apiResource("invoices", "InvoiceController");
                 Route::apiResource("tenders", "CreateTenderController");
