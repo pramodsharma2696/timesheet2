0b4d58fa89d27aec4f831ecd770f4608968aa8d2
Sagar Mahajan <sagar.mahajan@mindpooltech.net>
2024-04-22 19:00:54 +0530

    CORS issue

--- app/Http/Middleware/CorsMiddlewar.php
+++ app/Http/Middleware/CorsMiddlewar.php
@@ -13,15 +13,26 @@ class CorsMiddlewar
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next): Response
-    {
-        $response = $next($request);
+    // public function handle(Request $request, Closure $next): Response
+    // {
+    //     $response = $next($request);
 
-        // Add CORS headers to the response
-        $response->header('Access-Control-Allow-Origin', '*');
-        $response->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-        $response->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    //     // Add CORS headers to the response
+    //     $response->header('Access-Control-Allow-Origin', 'http://localhost:4900');
+    //     $response->header('Access-Control-Allow-Origin', '*');
+    //     $response->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    //     $response->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
-        return $response;
-    }
+    //     return $response;
+    // }
+    
+    public function handle($request, Closure $next)
+{
+    return $next($request)
+        ->header('Access-Control-Allow-Origin', '*')
+        ->header('Access-Control-Allow-Methods', '*')
+        ->header('Access-Control-Allow-Credentials', true)
+        ->header('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,X-Token-Auth,Authorization')
+        ->header('Accept', 'application/json');
+}
 }
