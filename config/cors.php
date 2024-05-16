e6b65caef1cb31302f0fe782796e24e9151592f5
pramod sharma <pramod.sharma@mindpooltech.in>
2024-05-16 14:07:29 +0530

    commit minor fix in cors

--- config/cors.php
+++ config/cors.php
@@ -1,11 +1,31 @@
 <?php
+// return [
+//     'paths' => ['*'],
+//     'allowed_methods' => ['*'],
+//     'allowed_origins' => ['https://rapisurv2-k6dl.vercel.app'], //add your allowed origins
+//     'allowed_origins_patterns' => [],
+//     'allowed_headers' => ['*'],
+//     'exposed_headers' => [],
+//     'max_age' => 0,
+//     'supports_credentials' => false,
+// ];
+
 return [
-    'paths' => ['*'],
+
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:4900'], //add your allowed origins
+
+    'allowed_origins' => ['https://rapisurv2-k6dl.vercel.app'],
+
     'allowed_origins_patterns' => [],
+
     'allowed_headers' => ['*'],
-    'exposed_headers' => [],
-    'max_age' => 0,
+
+    'exposed_headers' => false,
+
+    'max_age' => false,
+
     'supports_credentials' => false,
+
 ];
