db7120385bcc0e2ad8f378a57bfd65bcb5879942
pramod sharma <pramod.sharma@mindpooltech.in>
2024-07-11 10:22:28 +0530

    commit cors fix1

--- config/cors.php
+++ config/cors.php
@@ -1,22 +1,26 @@
 <?php
 /*
-    |--------------------------------------------------------------------------
-    | Cross-Origin Resource Sharing (CORS) Configuration
-    |--------------------------------------------------------------------------
-    |
-    | Here you may configure your settings for cross-origin resource sharing
-    | or "CORS". This determines what cross-origin operations may execute
-    | in web browsers. You are free to adjust these settings as needed.
-    |
-    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
-    |
-    */
+|--------------------------------------------------------------------------
+| Cross-Origin Resource Sharing (CORS) Configuration
+|--------------------------------------------------------------------------
+|
+| Here you may configure your settings for cross-origin resource sharing
+| or "CORS". This determines what cross-origin operations may execute
+| in web browsers. You are free to adjust these settings as needed.
+|
+| To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+|
+*/
 
 return [
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['https://rapisurv2-k6dl.vercel.app','https://qa-timesheet.rapisurv.com'],
+    'allowed_origins' => [
+        'https://rapisurv2-k6dl.vercel.app',
+        'https://qa-timesheet.rapisurv.com',
+        'https://qa.rapisurv.com'
+    ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
